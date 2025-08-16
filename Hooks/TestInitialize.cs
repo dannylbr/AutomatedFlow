b07c133b6527ce56ad3369773f20be3c69f1fac6
Daniel Soares <dannylbr@outlook.com>
2025-08-16 17:46:12 -0300

    Driver fixture

--- Hooks/TestInitialize.cs
+++ Hooks/TestInitialize.cs
@@ -2,9 +2,15 @@
 using Boa.Constrictor.Selenium;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Safari;
 using Reqnroll;
 using Reqnroll.BoDi;
 using System;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
 
 namespace AutomatedFlow.Hooks
 {
@@ -14,8 +20,13 @@ namespace AutomatedFlow.Hooks
         [BeforeScenario]
         public void BeforeScenario()
         {
-            ChromeOptions options = new();
-            //options.AddArgument("--headless");
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            var options = new ChromeOptions();
+            options.AddArguments("--test-type"
+                , "--start-maximized"
+                , "--no-sandbox"
+                , "--ignore-certificate-errors"
+                , "--headless");            
             var Actor = new Actor("AutomatedFlowActor", new ConsoleLogger());
             Actor.Can(BrowseTheWeb.With(new ChromeDriver(options)));
             objectContainer.RegisterInstanceAs(Actor);
