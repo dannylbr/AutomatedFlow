d5dc6fc0e19ec81817ee25b456495ac8d1f68f3e
Daniel Soares <dannylbr@outlook.com>
2025-08-17 13:48:51 -0300

    Corrected json and locators description

--- PageObjects/Home.cs
+++ PageObjects/Home.cs
@@ -7,11 +7,11 @@ namespace AutomatedFlow.PageObjects
     public static class Home
     {
         public const string Url = "https://sites.google.com/view/integraesol/";        
-        public static WebLocator Elegant => L("Elegant", By.XPath("//*[@id=\"h.3a4bb1d2f4063c83_0\"]/div/div/p/span"));
-        public static WebLocator Projects => L("", By.XPath("//*[@id=\"WDxLfe\"]/ul/li[2]/div[1]/div/a"));
-        public static WebLocator About => L("", By.XPath("//*[@id=\"WDxLfe\"]/ul/li[3]/div[1]/div/a"));
-        public static WebLocator Comments => L("", By.XPath("//*[@id=\"WDxLfe\"]/ul/li[4]/div[1]/div/a"));
-        public static WebLocator FirstLink => L("", By.XPath("//p/a/span"));
-        public static WebLocator Title => L("", By.ClassName("CGqCRe"));
+        public static WebLocator Elegant => L("Elegant text", By.XPath("//*[@id=\"h.3a4bb1d2f4063c83_0\"]/div/div/p/span"));
+        public static WebLocator Projects => L("Projects link", By.XPath("//*[@id=\"WDxLfe\"]/ul/li[2]/div[1]/div/a"));
+        public static WebLocator About => L("About link", By.XPath("//*[@id=\"WDxLfe\"]/ul/li[3]/div[1]/div/a"));
+        public static WebLocator Comments => L("Comments link", By.XPath("//*[@id=\"WDxLfe\"]/ul/li[4]/div[1]/div/a"));
+        public static WebLocator FirstLink => L("First link of the main page", By.XPath("//p/a/span"));
+        public static WebLocator Title => L("Page title", By.ClassName("CGqCRe"));
     }
 }
